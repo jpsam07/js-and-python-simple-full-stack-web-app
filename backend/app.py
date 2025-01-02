1c3a4ba3ac70e3e1e17693b68502505aee18a621
Joaquin Samson <samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.com>
2025-01-03 04:22:23 +0800

    5 - commented out CORS in app.py, modified the BASE_URL in App.jsx

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,8 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+# Comment the CORS config for the production because we are running the frontend and backend on the same server
+# CORS(app) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,33 +3,33 @@ import Navbar from "./components/Navbar";
 import UserGrid from "./components/UserGrid";
 import { useState } from "react";
 
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
     const [users, setUsers] = useState([]);
-  return (
-    <Stack minH={"100vh"}>
-      <Navbar setUsers={setUsers} />
+    return (
+      <Stack minH={"100vh"}>
+        <Navbar setUsers={setUsers} />
 
-      <Container maxW={"1200px"} my={4}></Container>
-      <Text
-      fontSize={{ base: "3x1", md: "50" }}
-      fontWeight={"bold"}
-      letterSpacing={"2px"}
-      textTransform={"uppercase"}
-      textAlign={"center"}
-      mb={8}
-      >
+        <Container maxW={"1200px"} my={4}></Container>
+        <Text
+        fontSize={{ base: "3x1", md: "50" }}
+        fontWeight={"bold"}
+        letterSpacing={"2px"}
+        textTransform={"uppercase"}
+        textAlign={"center"}
+        mb={8}
+        >
 
-        <Text 
-        as={"span"}
-        bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}
-        >Peak Gamer Peeps</Text>
-        🎮
-      </Text>
+          <Text 
+          as={"span"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}
+          >Peak Gamer Peeps</Text>
+          🎮
+        </Text>
 
-      <UserGrid users={users} setUsers={setUsers} />
-    </Stack>
+        <UserGrid users={users} setUsers={setUsers} />
+      </Stack>
   );
 }
 
